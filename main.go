64f7668742803b1b30ad6cfbec0a43a277d7506d
syprasha1994 <61225226+syprasha1994@users.noreply.github.com>
2020-02-27 18:10:34 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	current_time := time.Now().Local()
-	fmt.Fprintf(w, "Hello, You're running zarvis-example-go! %s\n\n", current_time.Format("2006-01-02 15:04:05"))
+	fmt.Fprintf(w, "Hello, You're running zarvis demo %s\n\n", current_time.Format("2006-01-02 15:04:05"))
 	for k, v := range r.Header {
 		fmt.Fprintf(w, "%s: %s\n", k, v)
 	}
